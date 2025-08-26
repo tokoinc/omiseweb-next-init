dca0d716d7478b68b1a2c966015f298ccb0630a0
omiseworks <kizawa@omiseweb.com>
2025-08-27 06:05:11 +0900

    Add files via upload

---
+++ tailwind.config.ts
@@ -0,0 +1,10 @@
+import type { Config } from 'tailwindcss';
+
+export default {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: { extend: {} },
+  plugins: [],
+} satisfies Config;
